4511d0992d52448854b622897421c1c4373f73a9
G <Q>
2019-08-31 18:54:16 -0500

    Added Tray Menu with Mainwindow functionality
    The Tray Icon doesn't work how I thought it would hence I will be changing how I will be using the tray and Main window

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, Tray ,BrowserWindow} = require('electron')
+const {app, Tray ,Menu,BrowserWindow} = require('electron')
 const path = require('path')
+const electron = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,17 +12,52 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: 'Copy History',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    show: true
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  const {x,y} = getWindowPosition();
+  mainWindow.setPosition(x,y);
   tray = new Tray('./icon/icon.png');
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Maximize', 
+      type: 'radio' ,
+      click: ()=>{
+        mainWindow.show();
+      }
+     
+    },
+    {
+      label: 'Minimize', 
+      type: 'radio' ,
+      click: ()=>{
+        mainWindow.hide();
+      }
+    }
+  ]);
+  tray.setToolTip('This is my application.');
+  
+
+  tray.setContextMenu(contextMenu);
+
+  getWindowPosition()
+
+  //when main window show
+  mainWindow.on('show', () => {
+    console.log('hello');
+    //mainWindow.setPosition(bounds.x,bounds.y);
+  });
+  //mainWindow.on('hide', () => {
+  //  tray.setHighlightMode('never');
+  //});
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -52,3 +88,11 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+function getWindowPosition(){
+  const windowBounds = mainWindow.getBounds();
+  const Screen = electron.screen;
+  const screenSize = Screen.getDisplayNearestPoint(Screen.getCursorScreenPoint()).workArea;
+  let x = Math.floor(screenSize.x + ((screenSize.width / 2) - (windowBounds.width / 2)));
+  let y = Math.floor(((screenSize.height + screenSize.y) / 2) - (windowBounds.height / 2));
+  return {x,y};
+};
